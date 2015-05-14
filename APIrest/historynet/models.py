ca687154f456e0a5611579a4bd1504831d0a656e
Francisco López Alarcón <francisco.lopez.a@usach.cl>
2015-05-14 03:52:30 -0300

    db 2
    Indentación error

--- APIrest/historynet/models.py
+++ APIrest/historynet/models.py
@@ -3,11 +3,12 @@ from django.db import models
 
 #Ejemplo de creacion de tablas en db
 #class TABLE_NAME(Models.Model)
+    #nombre_columna = models.(tipo de columna, consultar api https://docs.djangoproject.com/en/1.8/topics/db/models/#field-types ).
+
 class login(models.Model):
-	#nombre_columna = models.(tipo de columna, consultar api https://docs.djangoproject.com/en/1.8/topics/db/models/#field-types ).
 	user_name = models.CharField(max_length=10)
-    password = models.CharField(max_length=50)
-    email = models.CharField(max_length=100)
+	password = models.CharField(max_length=50)
+	email = models.CharField(max_length=100)
 
 
 
