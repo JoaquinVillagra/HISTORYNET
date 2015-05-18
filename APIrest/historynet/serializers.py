cee74c38f57320873bd806df7157b2bd004a6421
Francisco López Alarcón <francisco.lopez.a@usach.cl>
2015-05-18 02:24:04 -0300

    API test 6
    Al menos funcionó.
    http://46.101.184.198:8000/
    fingeso / fingeso

--- APIrest/historynet/serializers.py
+++ APIrest/historynet/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 class LoginSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Login
-        fields = ('url', 'user_name', 'password', 'email', 'level', 'estado', 'last_login')
+        fields = ('url', 'id', 'user_name', 'password', 'email', 'level', 'estado', 'last_login')
 
 
 class UsuarioSerializer(serializers.HyperlinkedModelSerializer):
