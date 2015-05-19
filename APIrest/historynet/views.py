6cad6eeb1d78b6dbd2b94a713546295739a499a3
Francisco López Alarcón <francisco.lopez.a@usach.cl>
2015-05-19 16:31:10 -0300

    API Commit 11
    Fix Lugar.

--- APIrest/historynet/views.py
+++ APIrest/historynet/views.py
@@ -2,13 +2,13 @@ from .models import Usuario, Lugar, Informacion_adicional
 from rest_framework import viewsets
 from .serializers import UsuarioSerializer, LugarSerializer, Informacion_adicionalSerializer
 
-class LoginViewSet(viewsets.ModelViewSet):
-    queryset = Login.objects.all()
-    serializer_class = LoginSerializer
-
 class UsuarioViewSet(viewsets.ModelViewSet):
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
+
+class LugarViewSet(viewsets.ModelViewSet):
+    queryset = Lugar.objects.all()
+    serializer_class = LugarSerializer
 
 class Informacion_adicionalViewSet(viewsets.ModelViewSet):
 	queryset = Informacion_adicional.objects.all()
