896f4ede63a33f4822ea9907380a7278e80049c3
Francisco López Alarcón <francisco.lopez.a@usach.cl>
2015-05-19 16:27:08 -0300

    API Commit 8
    fix  urls admin

--- APIrest/APIrest/urls.py
+++ APIrest/APIrest/urls.py
@@ -32,7 +32,7 @@ router.register(r'lugares', views.Informacion_adicionalViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', include(admin.site.urls))
     
 ]
