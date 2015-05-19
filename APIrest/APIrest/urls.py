809b38702beccdb5764ba87a8162160e54db1f60
Francisco López Alarcón <francisco.lopez.a@usach.cl>
2015-05-19 15:48:47 -0300

    admin site
    activando admin site

--- APIrest/APIrest/urls.py
+++ APIrest/APIrest/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 
 from django.conf.urls import url, include
+from django.contrib import admin
+admin.autodiscover()
+
 from rest_framework import routers
 from historynet import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
@@ -29,4 +33,6 @@ router.register(r'usuarios', views.UsuarioViewSet)
 urlpatterns = [
     url(r'^', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^admin/', include(admin.site.urls))
+    
 ]
