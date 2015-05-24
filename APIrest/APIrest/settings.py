db4ad384bb67416f8acb819ff7481f2fddbef0ac
Francisco López Alarcón <francisco.lopez.a@usach.cl>
2015-05-24 00:42:06 -0300

    permisos

--- APIrest/APIrest/settings.py
+++ APIrest/APIrest/settings.py
@@ -43,11 +43,16 @@ INSTALLED_APPS = (
     'rest_framework',
 )
 
+#REST_FRAMEWORK = {
+#    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+#    'PAGE_SIZE': 10
+#}
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
-    'PAGE_SIZE': 10
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        #'rest_framework.authentication.BasicAuthentication',
+        #'rest_framework.authentication.SessionAuthentication',
+    )
 }
-
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
